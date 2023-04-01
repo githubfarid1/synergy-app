52d391b4f2dd16aca27221290d09447d091fc71c
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-04-01 15:36:18 +0900

    update script

--- main.py
+++ main.py
@@ -115,6 +115,7 @@ class FileChooserFrame(ttk.Frame):
 			self.filename = filenametmp
 			if kwargs['sheetlist'] != None:
 				wb = openpyxl.load_workbook(filenametmp)
+				print(wb.sheetnames)
 				if type(kwargs['sheetlist']) == tuple:
 					for idx, sl in enumerate(kwargs['sheetlist']):
 						kwargs['sheetlist'][idx]['values'] = wb.sheetnames
