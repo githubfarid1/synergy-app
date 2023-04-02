4708536d0141ffbbdbe29c9ac9078724e7789718
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-04-02 09:55:41 +0900

    update script

--- tester/xlsm_tester.py
+++ tester/xlsm_tester.py
@@ -1,7 +1,14 @@
-from openpyxl import Workbook, load_workbook
+# from openpyxl import Workbook, load_workbook
 
-xlsfile = 'C:\\synergy-data-tester\\shipmentall\\xUSA Small Shipment Creation V12.20.xlsm'
+# xlsfile = 'C:\\synergy-data-tester\\shipmentall\\xUSA Small Shipment Creation V12.20.xlsm'
+# sname = 'Shipment summary'
+# workbook = load_workbook(filename=xlsfile, read_only=False, keep_vba=True, data_only=True)
+# worksheet = workbook[sname]
+# workbook.save(xlsfile)
+
+from xlwings import Workbook
+xlsfile = r'C:/synergy-data-tester/shipmentall/xUSA Small Shipment Creation V12.20.xlsm'
+newfile = r'C:/synergy-data-tester/shipmentall/yUSA Small Shipment Creation V12.20.xlsm'
 sname = 'Shipment summary'
-workbook = load_workbook(filename=xlsfile, read_only=False, keep_vba=True, data_only=True)
-worksheet = workbook[sname]
-workbook.save(xlsfile)
+wb = Workbook(xlsfile)
+wb.save(newfile)
