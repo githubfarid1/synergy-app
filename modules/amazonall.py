fc50f7228331119fdef7ab25af155f4592454183
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-04-02 16:33:16 +0900

    update script

--- modules/amazonall.py
+++ modules/amazonall.py
@@ -7,12 +7,18 @@ import json
 from random import randint
 from datetime import date, datetime, timedelta
 import amazon_lib as lib
-from subprocess import Popen
+import amazonship
+import logging
+from pathlib import Path
 
 if platform == "linux" or platform == "linux2":
     PYLOC = "python"
 elif platform == "win32":
     PYLOC = "python.exe"
+logger = logging.getLogger()
+logger.setLevel(logging.NOTSET)
+logger2 = logging.getLogger()
+logger2.setLevel(logging.NOTSET)
 
 def main():
     parser = argparse.ArgumentParser(description="Amazon Shipment")
@@ -41,24 +47,57 @@ def main():
         input(args.pdfoutput + " folder does not exist")
         sys.exit()
     strdate = str(date.today())
-    foldername = "{}{}_{}".format(args.pdfoutput + lib.file_delimeter(), 'shipment_creation', strdate) 
-    isExist = os.path.exists(foldername)
+    folderamazonship = "{}{}_{}".format(args.pdfoutput + lib.file_delimeter(), 'shipment_creation', strdate) 
+    isExist = os.path.exists(folderamazonship)
     if not isExist:
-        os.makedirs(foldername)
-    # print("Step 1: Shipment Creation", flush=True)
-    comlist=[PYLOC, "modules/amazonship.py", "-xls", '"' + args.xlsinput + '"', "-sname", '"' + args.shipsheet + '"', "-output", '"' + foldername + '"', "-cdata", '"' + args.chromedata + '"']
-    # Popen(comlist)
-    print(" ".join(comlist))
-    os.popen(" ".join(comlist))
-    foldername = "{}{}_{}".format(args.pdfoutput + lib.file_delimeter(), 'prior_notice', strdate) 
-    isExist = os.path.exists(foldername)
-    if not isExist:
-        os.makedirs(foldername)
+        os.makedirs(folderamazonship)
 
-    # print("Step 2: Prior Notice", flush=True)
-    comlist=[PYLOC, "modules/autofdapdf.py", "-i", '"' + args.xlsinput + '"', "-d", '"' + args.chromedata + '"', "-s", '"' + args.shipsheet + '"', "-dt", '"' + args.date + '"', "-o", '"' + foldername + '"']
-    # Popen(comlist)
-    os.popen(" ".join(comlist))
+    foldernamepn = "{}{}_{}".format(args.pdfoutput + lib.file_delimeter(), 'prior_notice', strdate) 
+    isExist = os.path.exists(foldernamepn)
+    if not isExist:
+        os.makedirs(foldernamepn)
+
+    file_handler = logging.FileHandler('logs/amazonship-err.log')
+    file_handler.setLevel(logging.ERROR)
+    file_handler_format = '%(asctime)s | %(levelname)s | %(lineno)d: %(message)s'
+    file_handler.setFormatter(logging.Formatter(file_handler_format))
+    logger.addHandler(file_handler)
+
+    file_handler2 = logging.FileHandler('logs/amazonship-info.log')
+    file_handler2.setLevel(logging.INFO)
+    # file_handler2_format = '%(asctime)s | %(levelname)s: %(message)s'
+    file_handler2_format = '%(asctime)s | %(levelname)s | %(lineno)d: %(message)s'
+    file_handler2.setFormatter(logging.Formatter(file_handler2_format))
+    logger2.addHandler(file_handler2)
+
+    logger2.info("###### Start ######")
+    logger2.info("Filename: {}\nSheet Name:{}\nPDF Output Folder:{}".format(args.xlsinput, args.shipsheet, args.pdfoutput))
+    maxrun = 10
+    for i in range(1, maxrun+1):
+        if i > 1:
+            print("Process will be reapeated")
+        try:    
+            shipment = amazonship.AmazonShipment(xlsfile=args.xlsinput, sname=args.shipsheet, chrome_data=args.chromedata, download_folder=args.pdfoutput)
+            shipment.data_sanitizer()
+            if len(shipment.datalist) == 0:
+                break
+            shipment.parse()
+        except Exception as e:
+            logger.error(e)
+            print("There is an error, check logs/amazonship-err.log")
+            shipment.workbook.save(shipment.xlsfile)
+            shipment.workbook.close()
+            if i == maxrun:
+                logger.error("Execution Limit reached, Please check the script")
+            continue
+        break
+    addressfile = Path("address.csv")
+    resultfile = lib.join_pdfs(source_folder=args.pdfoutput + lib.file_delimeter() + "combined" , output_folder = args.pdfoutput, tag='Labels')
+    if resultfile != "":
+        lib.add_page_numbers(resultfile)
+        lib.generate_xls_from_pdf(resultfile, addressfile)
+    input("End Process..")    
+
 
 
 if __name__ == '__main__':
