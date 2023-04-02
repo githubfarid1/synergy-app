014a4190a0286f02a801b72bdf6f1db43fb9dd36
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-04-03 07:34:33 +0900

    update script

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ elif platform == "win32":
 	from subprocess import CREATE_NEW_CONSOLE
 import json
 
-VERSION = "1.06"
+VERSION = "1.07"
 def getConfig():
 	file = open("setting.json", "r")
 	config = json.load(file)
