37716974a6ba3e8ade715bcf4974731cabecf909
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-04-03 07:01:00 +0900

    update script

---
+++ tester/copy_sheet.py
@@ -0,0 +1,22 @@
+from openpyxl import Workbook, load_workbook
+
+filename1 = "/home/farid/dev/python/synergy-github/data/sample/copy_sheet/xUSA Small Shipment Creation V12.20.xlsm"
+filename2 = "/home/farid/dev/python/synergy-github/data/sample/copy_sheet/April 01 Labels.xlsx"
+wb1 = load_workbook(filename=filename1, read_only=False, keep_vba=True, data_only=True)
+wb2 = load_workbook(filename=filename2, read_only=False, keep_vba=True, data_only=True)
+try:
+    del wb1['New1']
+except:
+    pass
+
+wb1.create_sheet('New1')
+ws1 = wb1['New1']
+ws2 = wb2["Sheet"]
+for i in range(1, ws2.max_row + 1):
+    cols = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')
+    for col in cols:
+        ws1['{}{}'.format(col, i)].value = ws2['{}{}'.format(col, i)].value
+        print(ws2['{}{}'.format(col, i)].value)
+
+wb1.save(filename1)
+wb1.close()
