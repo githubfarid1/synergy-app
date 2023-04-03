0e3cb0a1d65e71c035793766009622662a257fe8
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-04-03 11:53:53 +0900

    update script

--- tester/copy_sheet.py
+++ tester/copy_sheet.py
@@ -6,8 +6,8 @@ import amazon_lib as lib
 
 # filename1 = "/home/farid/dev/python/synergy-github/data/sample/copy_sheet/xUSA Small Shipment Creation V12.20.xlsm"
 # filename2 = "/home/farid/dev/python/synergy-github/data/sample/copy_sheet/April 01 Labels.xlsx"
-destination = r"C:/synergy-data-tester/copy_sheet/xUSA Small Shipment Creation V12.20.xlsm"
-source = r"C:/synergy-data-tester/copy_sheet/April 01 Labels.xlsx"
+destination = r"C:/synergy-data-tester/shipmentall/xUSA Small Shipment Creation V12.20.xlsm"
+source = r"C:/synergy-data-tester/shipmentall/shipment_creation_2023-04-02/April 02 Labels.xlsx"
 cols = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')
 sheetsource="Sheet"
 sheetdestination="Shipment labels summary"
