2db87463a9a0e7c4296fa2a9664f777df128bc65
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-04-11 12:27:33 +0900

    update script

--- modules/walmart_superstore.py
+++ modules/walmart_superstore.py
@@ -29,8 +29,8 @@ def browser_init():
     # options.add_argument("user-data-dir={}".format(config['chrome_user_data'])) 
     # options.add_argument("profile-directory={}".format(config['chrome_profile']))
     
-    options.add_argument("user-data-dir={}".format("C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data")) 
-    options.add_argument("profile-directory={}".format("Default5"))
+    options.add_argument("user-data-dir={}".format("C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data2")) 
+    options.add_argument("profile-directory={}".format("Default2"))
 
     options.add_argument('--no-sandbox')
     options.add_argument("--log-level=3")
