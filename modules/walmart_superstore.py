24d91744c57f9b1eb6a9f496648f2d4f1bac8fed
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-04-11 14:28:11 +0900

    update script

--- modules/walmart_superstore.py
+++ modules/walmart_superstore.py
@@ -62,6 +62,6 @@ for i in range(2, worksheet.max_row + 1):
         pg.scroll(randint(-100, 100))
         driver.get("https://google.com")
         time.sleep(randint(1, 5))
-        input("wait")
+        # input("wait")
         
          
