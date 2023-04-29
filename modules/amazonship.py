3b71d9154d8299dbc1bc3cf76772723550b10041
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-04-29 17:45:00 +0900

    update script

--- modules/amazonship.py
+++ modules/amazonship.py
@@ -986,6 +986,7 @@ def main():
         break
 
     for rlist in shipment.datareadylist:
+        print(folderamazonship, rlist['boxname'], rlist['shipid'][0:12], rlist['label'])
         extract_pdf(download_folder=folderamazonship, box=rlist['boxname'], shipment_id=rlist['shipid'][0:12], label=rlist['label'] )
     addressfile = Path("address.csv")
     resultfile = lib.join_pdfs(source_folder=folderamazonship + lib.file_delimeter() + "combined" , output_folder = folderamazonship, tag='Labels')
